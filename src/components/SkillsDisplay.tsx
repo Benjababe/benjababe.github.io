4c11d0c3423866d7d85853d7d640810cd573f8e6
Benjababe <bengohzy@gmail.com>
2023-09-20 20:03:07 +0800

    scroll skill display on view

--- src/components/SkillsDisplay.tsx
+++ src/components/SkillsDisplay.tsx
@@ -1,11 +1,40 @@
 import ScrollContainer from 'react-indiana-drag-scroll';
 import SkillIcon from './icons/SkillIcon';
+import { useEffect, useRef } from 'react';
 
 interface SkillsProps {
   skills: string[];
 }
 
 const SkillsDisplay = ({ skills }: SkillsProps) => {
+  let animatedOnce = false;
+  const skillContainerRef = useRef<HTMLElement>(null);
+
+  function previewSkills() {
+    setTimeout(() => {
+      skillContainerRef.current.scrollTo({
+        left: skillContainerRef.current.scrollWidth / 2,
+        behavior: 'smooth',
+      });
+      console.dir(skillContainerRef.current);
+      setTimeout(() => {
+        skillContainerRef.current.scrollTo({ left: 0, behavior: 'smooth' });
+      }, 1500);
+    }, 1000);
+  }
+
+  // When the skill block can be seen, scroll it to let
+  // user know it can be scrolled and isn't just clipping
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting && !animatedOnce) {
+        previewSkills();
+        animatedOnce = true;
+      }
+    });
+    observer.observe(skillContainerRef.current);
+  }, []);
+
   // sort skill alphabetically first
   // then get the skill image
   const imgArr = skills
@@ -16,7 +45,7 @@ const SkillsDisplay = ({ skills }: SkillsProps) => {
 
   return (
     <div className="light-text">
-      <ScrollContainer horizontal={true}>
+      <ScrollContainer horizontal={true} innerRef={skillContainerRef}>
         <div className="skill-description">{skills.join(' ')}</div>
         <div className="skill-block">{imgArr}</div>
       </ScrollContainer>
